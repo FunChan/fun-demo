df85b4e6c6faef00cb93fafb528d51c8f01d59a7
Fan <finer1010200912@163.com>
2014-12-10 00:20:03 +0800

    控制分支修复 BUG

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
+// 修改了一个 bug
 int main(void)
 {
 	printf("Hello GitHub!\n");
 	printf("Hello World!\n");
 	printf("Hello!\n");
 	printf("HelloWorld!\n");
-		
+	
 	return 0;
 }
